819acb4ddb4622728da4afa315d6e23ba745ea47
unknown <Newton254#>
2022-04-28 15:39:50 +0300

    tweak

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whienose.middleware.WhiteNoseMiddleware',
+    'whitenose.middleware.WhiteNoseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
